35adfae2475e5fb5e074a05bdf029b9a57a1ca8e
Brian Baingana <Brian.Baingana@windlogics.com>
2019-03-22 11:58:05 -0500

    Remove data files; add simulated data function; add basic evaluation

--- python/scripts/test.py
+++ python/scripts/test.py
@@ -2,29 +2,25 @@ from test_harness import test_data_generator
 from datetime import timedelta
 import pandas as pd
 import logging
+from test_harness import utils, evaluate_forecast
 
 
-def run(model=None):
-    test_data_gen = test_data_generator.TestDataGenerator()
-    done = False
-    results = None
+def run(predict_fun, test_data):
 
-    while not done:
+    test_data_gen = test_data_generator.TestDataGenerator(test_data=test_data)
+    results = pd.DataFrame()
+
+    while True:
         d = test_data_gen.next_runtime(lag=5)
 
         if d is not None:
             runtime = d['runtime']
             logging.info('Runtime: {}'.format(runtime))
 
-            load_1 = d['data']['load_1']['load']
-            forecast = persistence(runtime, load_1)
-
-            if results is None:
-                results = forecast
-            else:
-                results = results.append(forecast, ignore_index=True)
+            load = d['data']['load']
+            forecast = predict_fun(runtime, load)
+            results = results.append(forecast, ignore_index=True)
         else:
-            done = True
             return results
     return results
 
@@ -44,5 +40,9 @@ def persistence(runtime, load, horizon=24):
 
 
 if __name__ == '__main__':
-    results = run()
-    print(results.head(20))
+    test_data = utils.simulate_dataset()
+    results = run(persistence, test_data)
+    evaluation_metrics = evaluate_forecast.evaluate_forecast(results, test_data['load'])
+    results.to_csv('results.csv', index=False)
+    evaluation_metrics['hourly_metrics'].to_csv('hourly_error_metrics.csv', index=False)
+
--- python/src/test_harness/test_data_generator.py
+++ python/src/test_harness/test_data_generator.py
@@ -1,30 +1,43 @@
 import pandas as pd
 from datetime import timedelta
 import logging
-from pkg_resources import resource_filename
-import gzip
-import pickle
-
-TEST_DATA_FILE = resource_filename('test_harness', 'test_data.pklz')
 
 
 class TestDataGenerator(object):
 
-    def __init__(self):
-        self._start_time = '2018-08-20 08:00:00'
-        self._end_time = '2019-02-10 08:00:00'
+    def __init__(self, start_time='2018-08-20 08:00:00', end_time='2019-02-10 08:00:00',
+                 test_data=None):
+        """
+        Initializer
+
+        :param start_time: Earliest time to generate test  data
+        :param end_time: Latest time to generate test daya
+        :param test_data: A dictionary with keys ['load', 'gfs', 'nam'] and corresponding datatsets as
+                          dataframes
+        """
+        self._start_time = start_time
+        self._end_time = end_time
         self._runtimes = pd.date_range(start=self._start_time,
                                        end=self._end_time,
                                        freq='D', tz='UTC')
         self._steps = 0
-        file_handle = gzip.open(TEST_DATA_FILE, 'rb')
-        self._test_data = pickle.load(file_handle)
-        file_handle.close()
+        if test_data is not None:
+            self._test_data = test_data
+        else:
+            logging.error("No test data provided.")
 
-    def _filter_data(self, t, k='load_1', n_days=3, lag=30):
-        load = self._test_data[k]['load']
-        gfs = self._test_data[k]['gfs']
-        nam = self._test_data[k]['nam']
+    def _filter_data(self, t, n_days=3, lag=30):
+        """
+        Filters load, GFS, and NAM datasets
+
+        :param t: Current runtime
+        :param n_days: Recency of latest available data
+        :param lag: How far back to fetch data
+        :return: A tuple comprising the filtered load, GFS, and NAM data
+        """
+        load = self._test_data['load']
+        gfs = self._test_data['gfs']
+        nam = self._test_data['nam']
         load = load[(load['validtime'] <= t - timedelta(days=n_days)) &
                     (load['validtime'] >= t - timedelta(days=n_days+lag))]
         gfs = gfs[(gfs['runtime'] <= t) & (gfs['runtime'] >= t - timedelta(days=lag))]
@@ -33,7 +46,7 @@ class TestDataGenerator(object):
 
     def next_runtime(self, lag=30):
         """
-        :param lag: History length in days for which to fetch data
+        :param lag: How far back (in days) to fetch data
         :return: Dictionary with runtime, load, and weather data going back lag days
         """
         if self._steps >= len(self._runtimes):
@@ -41,12 +54,10 @@ class TestDataGenerator(object):
             return None
 
         t = self._runtimes[self._steps]
-        load_1_load, load_1_gfs, load_1_nam = self._filter_data(t, k='load_1', n_days=3, lag=lag)
-        load_12_load, load_12_gfs, load_12_nam = self._filter_data(t, k='load_12', n_days=3, lag=lag)
-        load_51_load, load_51_gfs, load_51_nam = self._filter_data(t, k='load_51', n_days=3, lag=lag)
+        load, gfs, nam = self._filter_data(t, n_days=3, lag=lag)
 
         self._steps += 1
         return {'runtime': t,
-                'data': {'load_1': {'load': load_1_load, 'gfs': load_1_gfs, 'nam': load_1_nam},
-                         'load_12': {'load': load_12_load, 'gfs': load_12_gfs, 'nam': load_12_nam},
-                         'load_51': {'load': load_51_load, 'gfs': load_51_gfs, 'nam': load_51_nam}}}
+                'data': {'load': load,
+                         'gfs': gfs,
+                         'nam': nam}}
